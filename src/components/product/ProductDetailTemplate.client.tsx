ba545f137dc5a7369ab8e05c3c951e7a7ad5e0af
y.pham <y.pham@bluecomgroup.com>
2023-03-25 21:22:25 +0700

    Product detail page - variants

--- src/components/product/ProductDetailTemplate.client.tsx
+++ src/components/product/ProductDetailTemplate.client.tsx
@@ -1,8 +1,11 @@
 import { BuilderComponent, builder } from '@builder.io/react'
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import {
+  isBrowser,
   useProductOptions,
-  useCart
+  useCart,
+  useUrl,
+  OptionWithValues
 } from '@shopify/hydrogen';
 import type {
   Product
@@ -13,17 +16,23 @@ builder.init('679c25f761c647f2a8e6bf979c2a6820')
 const MODEL_NAME = 'product-template';
 
 export function ProductDetailTemplate({
-  product,
-  ...props
+  product
 }: {
   product: Product;
 }) {
+  const { linesAdd } = useCart();
+  const [quantity, setQuantity] = useState(1);
+  const {options, setSelectedOption, selectedOptions, selectedVariant} = useProductOptions();
+  const variantId = selectedVariant?.id ? selectedVariant?.id : '';
+  const {pathname, search} = useUrl();
+  const [params, setParams] = useState(new URLSearchParams(search));
+
   if (!product) {
     return null;
   }
 
+  // Load page builder content
   const [builderContentJson, setBuilderContentJson] = useState(undefined)
-
   useEffect(() => { 
     builder.get(MODEL_NAME, {
       query: {
@@ -32,19 +41,95 @@ export function ProductDetailTemplate({
           someNumber: { $ne: 1 }
         }
       }
-    })
-      .promise().then(setBuilderContentJson)
+    }).promise().then(setBuilderContentJson)
   }, [])
+  
+  useEffect(() => {
+    (options as OptionWithValues[]).map(({name, values}) => {
+      if (!params) return;
+      const currentValue = params.get(name.toLowerCase()) || null;
+      if (currentValue) {
+        const matchedValue = values.filter(
+          (value) => encodeURIComponent(value.toLowerCase()) === currentValue,
+        );
+        setSelectedOption(name, matchedValue[0]);
+      } else {
+        params.set(
+          encodeURIComponent(name.toLowerCase()),
+          encodeURIComponent(selectedOptions![name]!.toLowerCase()),
+        ),
+          window.history.replaceState(
+            null,
+            '',
+            `${pathname}?${params.toString()}`,
+          );
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  const { linesAdd } = useCart();
-  const [quantity, setQuantity] = useState(1);
-  const {selectedVariant} = useProductOptions();
-  const variantId = selectedVariant?.id ? selectedVariant?.id : '';
+  //Add "checked" class for selected Variant when loaded page
+  useEffect(() => {
+    const productVariants = document.getElementsByClassName('variant-item');
 
-  const handleUpdateQuantity = (event) => {
+    for (const element of productVariants) {
+      const nameVariants = element.querySelector('.variant-name span')?.textContent;
+      const variantsValues = element.querySelectorAll('.variants-values label');
+
+      for(const item of variantsValues) {
+        if(typeof nameVariants === 'string') {
+          if(item.getElementsByTagName('span')[0].innerText == selectedOptions![nameVariants]) {
+            item.classList.add('checked');
+          } else {
+            item.classList.remove('checked');
+          }
+        }
+      }
+    };
+  }, [selectedOptions, builderContentJson]);
+
+  //Handle update variants
+  const handleUpdateVariants = useCallback(
+    (event: any) => {
+      const labelParent = event.target.parentElement;
+      const variantsValues = event.target.closest('.variants-values');
+      const listLabel = variantsValues.getElementsByTagName('label');
+
+      const value = event.target.innerText;
+      const name = labelParent.getAttribute('for');
+      setSelectedOption(name, value);
+
+      for (const item of listLabel) {
+        if(item.getElementsByTagName('span')[0].innerText === value) {
+          item.classList.add('checked');
+        } else {
+          item.classList.remove('checked');
+        }
+      };
+
+      if (!params) return;
+      params.set(
+        encodeURIComponent(name.toLowerCase()),
+        encodeURIComponent(value.toLowerCase()),
+      );
+      
+      if (isBrowser()) {
+        window.history.replaceState(
+          null,
+          '',
+          `${pathname}?${params.toString()}`,
+        );
+      }
+    },
+    [setSelectedOption, params, pathname]
+  );
+
+  // Handle update quantity
+  const handleUpdateQuantity = (event: any) => {
     setQuantity(parseInt(event.target.value));
   }
 
+  // Handle add to cart item
   const handleAddItem = () => {
     linesAdd([
       {
@@ -54,12 +139,21 @@ export function ProductDetailTemplate({
     ]);
   };
 
+  let my_options: []
+  options?.forEach((result) => {
+      //options.push({value: result?.value, name: result?.name})
+  })
+  console.log(options);
+
   return (
     <BuilderComponent model={MODEL_NAME} content={builderContentJson}
       data={{
         products: product,
+        variantsOptions: options,
+        selectedVariant: selectedVariant,
         addToCart: () => handleAddItem(),
-        updateQuantity: (event) => handleUpdateQuantity(event)
+        updateQuantity: (event: any) => handleUpdateQuantity(event),
+        updateVariants: (event: any) => handleUpdateVariants(event)
       }}
     />
   )
